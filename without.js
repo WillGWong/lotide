fd2c67d8bdaf3cb8892537c85621f6289cc8e023
Willey Wong <willeyw@gmail.com>
2019-07-24 01:26:58 +0000

    added without function

--- without.js
+++ without.js
@@ -16,17 +16,15 @@ const assertArraysEqual = function(actual, expected) {
 }
 
 const without = function(source, itemsToRemove){
-  let newArr = []
   for (i = 0; i < source.length; i++){
     for (j = 0; j < itemsToRemove.length; j++){
-      if (itemsToRemove[j] !== source[i]) {
-        newArr.push(source[i]);
-        break;
+      if (itemsToRemove[j] === source[i]) {
+        source.splice(i,1);
       }
     }
   }
-return newArr
+return source
 }
 
-console.log(without([1, 2, 3], [1]))
-console.log(without(["1", "2", "3"], [1, 2, "3"]))
+assertArraysEqual(without([1, 2, 3], [1]), [2,3])
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"])
