a46984c15befc88f98219b2c5a3b409415efdaf3
Willey Wong <willeyw@gmail.com>
2019-07-25 20:49:43 +0000

    adeed map function

---
+++ map.js
@@ -0,0 +1,30 @@
+const eqArrays = function(firstArr, secondArr) {
+  for (let i = 0; i < firstArr.length; i++) {
+    if (firstArr[i] !== secondArr[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  if (eqArrays(actual, expected) === true) {
+    console.log(`✅✅✅Assertion passed: "${actual}" === "${expected}"`);
+  } else {
+    console.log(`🛑🛑🛑Assertion failed: "${actual}" !== "${expected}"`);
+  }
+};
+
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item))
+  }
+  return results;
+}
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
