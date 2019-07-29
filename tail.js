2995d0f2a13afe9f7aad822c6b466319ebaa1193
Willey Wong <willeyw@gmail.com>
2019-07-29 20:13:58 +0000

    refactored tail and created tailtest

--- tail.js
+++ tail.js
@@ -1,18 +1,6 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅Assertion passed: "${actual}" === "${expected}"`);
-  } else {
-    console.log(`🛑🛑🛑Assertion failed: "${actual}" !== "${expected}"`);
-  }
-};
 const tail = function(array) {
   array.shift();
   return array;
 };
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+module.exports = tail;
